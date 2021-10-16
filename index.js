6e1335fed62a1bfc4574e5863a1bee10907858b0
Muhamad Ramdani <59561245+dannsdb@users.noreply.github.com>
2021-10-17 05:30:54 +0700

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-const randomImages = require('./src/kucing')
-const fs = require('fs')
-const path = require('path')
-const chalk = require('chalk')
-const outputFile = path.join(__dirname, 'data/', 'data.json')
-const baseUrl = "https://r-images.dannsdb.id"
