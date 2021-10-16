e1223329d00ecdd11bcd40b166bf62407566e442
Muhamad Ramdani <59561245+dannsdb@users.noreply.github.com>
2021-10-16 21:53:53 +0000

    Add files via upload

---
+++ api/memesuki/index.js
@@ -0,0 +1,11 @@
+const path = require('path')
+const fs = require('fs').promises
+const fileJson = path.join(__dirname, '..', '..', 'data', 'Memesuki.json')
+
+module.exports = async (_, res) => {
+    const data = await fs.readFile(fileJson, 'utf-8')
+    const randomImages = JSON.parse(data)
+    const hasil = {"results" : randomImages}
+
+    res.status(200).send(hasil)
+}
