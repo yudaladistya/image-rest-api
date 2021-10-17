350860d6f578a85f394256dcd674c126a4d7fe88
Muhamad Ramdani <59561245+dannsdb@users.noreply.github.com>
2021-10-17 18:52:40 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 
+let express = require('express'), path = require('path'), cookieParser = require('cookie-parser'), logger = require('morgan')
+let indexRouter = require('./routes/index')
+let PORT = process.env.PORT || 8080
+
+const app = express()
+app.set('json spaces', 2)
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+app.use('/', indexRouter)
+app.get('*', function(req, res){
+    res.status(404).json(global.status.error)
+})
+
+app.listen(PORT, () => console.log(`Server is running in port ${PORT}`))
