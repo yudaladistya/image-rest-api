e5c4fa1aa75b1690d195a3d37f161ac3d34d55b4
Muhamad Ramdani <59561245+dannsdb@users.noreply.github.com>
2021-10-17 20:56:15 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,8 @@ router.get('/', (req, res) => {
     res.sendFile(process.cwd() + '/public/index.html')
 })
 
+router.get('../api', (req, res) => {
+	res.redirect('https://r-images.dannsdb.id')
+})
+
 module.exports = router
