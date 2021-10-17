55f6cf67e0b8626cf997385d81d96d06fb06191d
Muhamad Ramdani <59561245+dannsdb@users.noreply.github.com>
2021-10-17 18:46:15 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,1 @@
-const fs = require('fs')
-const path = require('path')
-const chalk = require('chalk')
-const outputFile = path.join(__dirname, 'data/', 'data.json')
-const baseUrl = "https://r-images.dannsdb.id"
+
