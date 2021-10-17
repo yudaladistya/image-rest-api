41554de7a7910a72d6f145931c53ce3a7b7f76df
Muhamad Ramdani <59561245+dannsdb@users.noreply.github.com>
2021-10-17 20:51:42 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 let express = require('express'), router = express.Router()
 
-router.get('/', (req, res) => {
+router.get('r-images.dannsdb.id', (req, res) => {
     res.sendFile(process.cwd() + '/public/index.html')
 })
 
