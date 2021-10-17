44ca333254a3384785239e591f0df1ae210da6d6
Muhamad Ramdani <59561245+dannsdb@users.noreply.github.com>
2021-10-17 21:19:05 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,12 @@
-var express = require('express');
-var app = express();
+let express = require('express'), router = express.Router(), favicon = require('serve-favicon')
+router.use(favicon(process.cwd() + '/'))
 
-// set up rate limiter: maximum of five requests per minute
-var RateLimit = require('express-rate-limit');
-var limiter = new RateLimit({
-  windowMs: 1*60*1000, // 1 minute
-  max: 5
-});
+router.get('/', (req, res) => {
+    res.sendFile(process.cwd() + '/public/index.html')
+})
 
-var fs = require('fs'),
-    http = require('http'),
-    url = require('url');
+router.get('/api', (req, res) => {
+	res.redirect('https://r-images.dannsdb.id')
+})
 
-var server = http.createServer(function(req, res) {
-  let path = url.parse(req.url, true).query.path;
-
-  // BAD: This could read any file on the file system
-  res.write(fs.readFileSync(path));
-
-  // BAD: This could still read any file on the file system
-  res.write(fs.readFileSync("/public/index.html" + path));
-});
+module.exports = router
