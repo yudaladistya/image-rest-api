319a89c901ff23f2067d052f4055b885f857557c
Muhamad Ramdani <59561245+dannsdb@users.noreply.github.com>
2021-10-17 21:40:21 +0700

    Update index.js

--- api/wpestetik/index.js
+++ api/wpestetik/index.js
@@ -1,11 +1,13 @@
 const path = require('path')
 const fs = require('fs').promises
-const fileJson = path.join(__dirname, '..', '..', 'data', 'Wpestetik.json')
+const fileJson = path.join(__dirname, '..', '..', 'data', 'WpEstetik.json')
 
 module.exports = async (_, res) => {
     const data = await fs.readFile(fileJson, 'utf-8')
     const randomImages = JSON.parse(data)
-    const hasil = {"results" : randomImages}
+    const hasil = {
+        "results": randomImages
+    }
 
     res.status(200).send(hasil)
 }
